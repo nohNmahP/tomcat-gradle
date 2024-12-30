b22f02a10267249ec15a357d9c9607fb6044fe13
Tên Thien Alice <thienngo.081003@gmail.com>
2024-12-30 15:02:55 +0700

    first commit

---
+++ tomcat/src/main/java/com/example/tomcat/WebController.java
@@ -0,0 +1,15 @@
+package com.example.tomcat;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class WebController {
+    @GetMapping("/")
+    public String getMessage()
+    {
+        return "Spring Boot Application running on Tomcat server!!";
+    }
+}
