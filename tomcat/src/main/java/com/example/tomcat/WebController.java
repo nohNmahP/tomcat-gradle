7165b1c755f080e408b7da90e687969d7c26e4b5
Phạm Hoàng Nhơn <everysecondcounts179@gmail.com>
2025-09-25 11:51:38 +0700

    Update WebController.java

--- tomcat/src/main/java/com/example/tomcat/WebController.java
+++ tomcat/src/main/java/com/example/tomcat/WebController.java
@@ -1,15 +1,17 @@
 package com.example.tomcat;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
 public class WebController {
+
+    @Value("${app.message}")
+    private String message;
+
     @GetMapping("/")
-    public String getMessage()
-    {
-        return "Spring Boot Application running on Tomcat server dfdsafsdasadfsda!!";
+    public String getMessage() {
+        return message;
     }
 }
